6d0894930b9ea950b2afba6537d8c04e0e2bb166
Ayush Kumar <68886542+Hi-Ayush@users.noreply.github.com>
2020-11-14 15:34:39 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">Tinder Clone</h1>
+      <p data-ns-test="project-description">You know what tinder is!</p>
+    </>
+  );
+};
 
 export default App;
